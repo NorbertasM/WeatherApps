cfc7fbf25dfe1c3d5fc04265f40b0655b5c91e0b
norbertasm <murauskasnorbertas@gmail.com>
2022-04-12 17:27:22 +0300

    add google maps support, button to switch between maps and graphs

--- Weather/js.js
+++ Weather/js.js
@@ -38,7 +38,6 @@ const getTime = (unix) => {
 }
 
 const getDayLength = (unix) => {
-  //might need to fix
   const hours = parseInt(unix / 3600)
   const minutes = parseInt(unix % 3600 / 60)
   const seconds = unix % 60
@@ -46,23 +45,30 @@ const getDayLength = (unix) => {
   return `${hours}:${minutes}:${seconds}`
 }
 
-// setInterval(() => {
-//   const time = new Date()
-//   const month = time.getMonth()
-//   const date = time.getDate()
-//   const day = time.getDay()
-//   const hour = time.getHours()
-//   const hours = hour >= 13 ? hour % 12 : hour
-//   const minutes = time.getMinutes()
-//   const ampm = hour >= 12 ? 'PM' : 'AM'
-//   timeEl.innerHTML =
-//     (hours < 10 ? '0' + hours : hours) +
-//     ':' +
-//     (minutes < 10 ? '0' + minutes : minutes) +
-//     ' ' +
-//     `<span id='am-pm'>${ampm}</span>`
-//   dateEl.innerHTML = days[day - 1] + ', ' + date + ' ' + months[month]
-// }, 1000)
+function initMap() {
+  const myLatlng = { lat: -25.363, lng: 131.044 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatlng,
+    draggableCursor: 'default'
+  });
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        map.setCenter(initialLocation);
+    });
+}
+  map.addListener("click", (mapsMouseEvent) => {
+    const lon = mapsMouseEvent.latLng.lng()
+    const lat = mapsMouseEvent.latLng.lat()
+
+    fetchForecast({
+      lon,
+      lat,
+    })
+  });
+}
 
 const fetchForecast = async ({ lon, lat }) => {
   try {
@@ -88,25 +94,61 @@ const fetchWeather = async (input) => {
         const { icon, description } = data.weather[0]
         const { feels_like, temp, humidity } = data.main
         const { deg, speed } = data.wind
-        document.querySelector('.city').innerText = `Oras mieste ${name}`
-        document.querySelector('.temp').innerText = `${temp}°C`
-        document.querySelector('.feelsLike').innerText = `Pojūtis ${feels_like}°C`
-        document.querySelector('.description').innerText = description
-        document.querySelector('.humidity').innerText = `Oro dregnumas: ${humidity}%`
-        document.querySelector('.wind').innerText = `Vejo greitis: ${speed}m/s`
-        document.querySelector('.sunrise').innerText = `Saulė teka: ${getTime(sunrise)}`
-        document.querySelector('.sunset').innerText = `Saulė leidžiasi: ${getTime(sunset)}`
-        document.querySelector('.daylength').innerText = `Dienos ilgumas: ${getDayLength(sunset - sunrise)}`
-        document.querySelector('.windDirection').innerText = `Vėjo kryptis: ${getWindDirection(deg)}`
-        document.querySelector('.icon').src = `http://openweathermap.org/img/wn/${icon}@2x.png`
-        document.querySelector('.weather').classList.remove('loading')
-        document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`
+        showCurrent({
+          name,
+          temp,
+          feels_like,
+          description,
+          humidity,
+          speed,
+          sunrise,
+          sunset,
+          deg,
+          icon
+        })
+
         fetchForecast(coord)
       }
     }
   } catch (error) {
     console.log(error)
   }
+}
+
+const showCurrent = ({ name, temp, feels_like, description, humidity, speed, sunrise, sunset, deg, icon }) => {
+  if (name) {
+    document.querySelector('.city').innerText = `Oras mieste ${name}`
+  }
+  document.querySelector('.temp').innerText = `${temp}°C`
+  document.querySelector('.feelsLike').innerText = `Pojūtis ${feels_like}°C`
+  document.querySelector('.description').innerText = description
+  document.querySelector('.humidity').innerText = `Oro dregnumas: ${humidity}%`
+  document.querySelector('.wind').innerText = `Vejo greitis: ${speed}m/s`
+  document.querySelector('.sunrise').innerText = `Saulė teka: ${getTime(sunrise)}`
+  document.querySelector('.sunset').innerText = `Saulė leidžiasi: ${getTime(sunset)}`
+  document.querySelector('.daylength').innerText = `Dienos ilgumas: ${getDayLength(sunset - sunrise)}`
+  document.querySelector('.windDirection').innerText = `Vėjo kryptis: ${getWindDirection(deg)}`
+  document.querySelector('.icon').src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+  document.querySelector('.weather').classList.remove('loading')
+  document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`
+}
+
+const changeToGraphs = () => {
+  const button = document.querySelector('.button')
+  button.innerText = 'Prognozė'
+  button.removeEventListener('click', changeToGraphs)
+  button.addEventListener('click', changeToForecast)
+
+  document.querySelector('.inputs-container').classList.add('loading')
+}
+
+const changeToForecast = () => {
+  const button = document.querySelector('.button')
+  button.innerText = 'Grafikai'
+  button.removeEventListener('click', changeToForecast)
+  button.addEventListener('click', changeToGraphs)
+
+  document.querySelector('.inputs-container').classList.remove('loading')
 }
 
 const showWeatherData = (info) => {
@@ -117,32 +159,51 @@ const showWeatherData = (info) => {
   const d = new Date()
   const dayName = days[d.getDay()]
   
-  info.daily.map(day => {
-    const container = document.createElement('div')
-    container.setAttribute('class', 'weather-forecast-item')
-    weatherForecast.appendChild(container)
+  if (!document.querySelector('.city').innerText) {
+    showCurrent({
+      temp: info.current.temp,
+      feels_like: info.current.feels_like,
+      description: info.current.weather[0]?.description,
+      humidity: info.current.humidity,
+      speed: info.current.wind_speed,
+      sunrise: info.current.sunrise,
+      sunset: info.current.sunset,
+      deg: info.current.wind_deg,
+      icon: info.current.weather[0]?.icon,
+    })
+  }
 
-    const dayContainer = document.createElement('div')
-    dayContainer.setAttribute('class', 'day')
-    dayContainer.innerText = dayName
-    container.appendChild(dayContainer)
+  if (info?.daily) {
+    info.daily.map(day => {
+      const container = document.createElement('div')
+      container.setAttribute('class', 'weather-forecast-item')
+      weatherForecast.appendChild(container)
+
+      const dayContainer = document.createElement('div')
+      dayContainer.setAttribute('class', 'day')
+      dayContainer.innerText = dayName
+      container.appendChild(dayContainer)
+      
+      const weatherImg = document.createElement('img')
+      weatherImg.setAttribute('src', `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`)
+      weatherImg.setAttribute('alt', 'weather-icon')
+      container.appendChild(weatherImg)
+
+      const dayWeather = document.createElement('div')
+      dayWeather.setAttribute('class', 'temperature')
+      dayWeather.innerText = `Diena ${day.temp.day}°C`
+      container.appendChild(dayWeather)
+      
+      const nightWeather = document.createElement('div')
+      nightWeather.setAttribute('class', 'temperature')
+      nightWeather.innerText = `Naktis ${day.temp.night}°C`
+      container.appendChild(nightWeather)
+    })
+
+    const button = document.querySelector('.button')
     
-    const weatherImg = document.createElement('img')
-    weatherImg.setAttribute('src', `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`)
-    weatherImg.setAttribute('alt', 'weather-icon')
-    container.appendChild(weatherImg)
-
-    const dayWeather = document.createElement('div')
-    dayWeather.setAttribute('class', 'temperature')
-    dayWeather.innerText = `Diena ${day.temp.day}°C`
-    container.appendChild(dayWeather)
-    
-    const nightWeather = document.createElement('div')
-    nightWeather.setAttribute('class', 'temperature')
-    nightWeather.innerText = `Naktis ${day.temp.night}°C`
-    container.appendChild(nightWeather)
-  })
-
+    button.addEventListener('click', changeToGraphs)
+  }
 }
 
 document.querySelector('.search-bar').addEventListener('keyup', function (event) {
